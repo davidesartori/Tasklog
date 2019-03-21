d5e3aaf0af21bad026782a190441b4c14df9b04d
davidesartori <davide.sartori.10@gmail.com>
2019-03-21 22:52:23 +0100

    first version

--- tasklog.py
+++ tasklog.py
@@ -1,9 +1,19 @@
 import firebase_admin
 from firebase_admin import credentials
 from firebase_admin import db
+import mysql.connector
+
+import datetime
 
 
-def connection():
+def database_query(cursor, query):
+    cursor.execute(query)
+    records = cursor.fetchall()
+
+    return records
+
+
+def firebase_connection():
     cred = credentials.Certificate('admin-sdk.json')
 
     firebase_admin.initialize_app(cred, {
@@ -11,7 +21,7 @@ def connection():
     })
 
 
-def update(ref_name, db_structure):
+def firebase_update(ref_name, db_structure):
     ref = db.reference(ref_name)
 
     ref.update(
@@ -20,9 +30,38 @@ def update(ref_name, db_structure):
 
 
 if __name__ == "__main__":
-    connection()
+    db_username = ""
+    db_password = ""
+    db_host = "localhost"
+    database = ""
+    db_table = ""
+    rows = ["process", "timestamp", "event", "level"]
+    id_in_table = True
 
-    db_ref = "orario"
-    structure = {'event': 'caricamento completato'}
+    processes = {}
 
-    update(db_ref, structure)
+    connection = mysql.connector.connect(user=db_username, password=db_password, host=db_host,
+                                         database=database)
+    database_cursor = connection.cursor(buffered=True)
+
+    result_set = database_query(database_cursor, "select * from " + db_table + " order by timestamp desc")
+
+    for result in result_set:
+        process = {}
+        for i in range(2, len(rows) + 1):
+            if type(result[i]) is datetime.datetime:
+                process[rows[i - 1]] = str(result[i])
+            else:
+                process[rows[i - 1]] = result[i]
+
+        if result[1] not in processes.keys():
+            processes[result[1]] = process
+
+    # firebase
+
+    firebase_connection()
+
+    db_ref = "/"
+    structure = processes
+
+    firebase_update(db_ref, structure)
